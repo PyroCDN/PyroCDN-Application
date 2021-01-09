517918dcb477a7f5cb8c1e851ccb744ca2705e8b
Luke-6723 <luke@ichigo.uk>
2021-01-09 04:19:11 +0000

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { QMainWindow, QWidget, QLabel, FlexLayout, QPushButton, QIcon } from '@nodegui/nodegui';
+
+const win = new QMainWindow();
+win.setWindowTitle("PyroCDN Uploader");
+
+const centralWidget = new QWidget();
+centralWidget.setObjectName("mainWindow");
+const rootLayout = new FlexLayout();
+centralWidget.setLayout(rootLayout);
+
+win.setCentralWidget(centralWidget);
+win.setStyleSheet(
+  `
+    #mainWindow {
+      background-color: #009688;
+      width: 100vw;
+      height: 100vh;
+    }
+  `
+);
+win.setFixedSize(1280, 720)
+win.show();
+
+(global as any).win = win;
